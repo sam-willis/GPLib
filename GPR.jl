1341160c6fd9bc6b968a29cf76a2b2112a8377d0
Sam <git@mail.samw.cc>
2022-03-16 23:57:06 +0000

    create some class stuff

--- GPR.jl
+++ GPR.jl
@@ -8,13 +8,25 @@ mutable struct GPRHyperParameters
     l:: Number
 end
 
-#%% write the basic equations we need for GPR + optimisation
-K(x, y, θ) = exp.(-abs.(x .- y').^2 ./ (2*θ.l^2))
-Kxx_noise(x, θ) = K(x, x, θ) + I*θ.σ^2
-mean(x, y, θ, x_new) = K(x_new, x, θ) * inv(Kxx_noise(x, θ)) * y
-cov(x, θ, x_new) = K(x_new, x_new, θ) - K(x_new, x, θ) * inv(Kxx_noise(x, θ)) * K(x_new, x, θ)'
-loglik(x, y, θ) = -0.5*y'*inv(Kxx_noise(x, θ))*y - 0.5*log(det(Kxx_noise(x, θ))) - 0.5*length(x)*log(2*pi)
-g(θ) = gradient(θ -> loglik(x, y, θ), θ)[1]
+struct GPR
+    K:: Function
+    mean:: Function
+    cov:: Function
+    loglik:: Function
+    θ:: GPRHyperParameters
+end
+
+#%% Helper functions
+RBFKernel(x, y, θ) = exp.(-abs.(x .- y').^2 ./ (2*θ.l^2))
+
+function createGPR(K, θ)
+    # extend Kernel.θ with σ and make that GPR.θ
+    Kxx_noise(x, θ) = K(x, x, θ) + I*θ.σ^2
+    mean(x, y, θ, x_new) = K(x_new, x, θ) * inv(Kxx_noise(x, θ)) * y
+    cov(x, θ, x_new) = K(x_new, x_new, θ) - K(x_new, x, θ) * inv(Kxx_noise(x, θ)) * K(x_new, x, θ)'
+    loglik(x, y, θ) = -0.5*y'*inv(Kxx_noise(x, θ))*y - 0.5*log(det(Kxx_noise(x, θ))) - 0.5*length(x)*log(2*pi)
+    GPR(K, mean, cov, loglik, θ)
+end
 
 function grad_step!(g, θ, alpha)
     grads = g(θ)
@@ -25,22 +37,30 @@ function grad_step!(g, θ, alpha)
     end
 end
 
+function optimise!(gpr:: GPR, x, y, n_steps=1000, alpha=0.001)
+    g(θ) = gradient(θ -> gpr.loglik(x, y, θ), θ)[1]
+    for _=1:n_steps
+        grad_step!(g, gpr.θ, alpha)
+    end
+end
+
 #%% create a problem
 x = collect(1:10); 
 y = x/10 + 0.5 * rand(size(x, 1))
 x_test = collect(1:0.01:20)
+
 θ = GPRHyperParameters(0.1, 1) # initial parameters - we gotta start somewhere
+gpr = createGPR(RBFKernel, θ)
 println(θ)
 
 #%% basic optimisation
-for i=1:1000
-    grad_step!(g, θ, 0.001)
-end
+
+optimise!(gpr, x, y)
 println(θ)
 
 #%% plot prediction vs training data
-mean_test = mean(x, y, θ, x_test)
-std_test = sqrt.(diag(cov(x, θ, x_test)))
+mean_test = gpr.mean(x, y, θ, x_test)
+std_test = sqrt.(diag(gpr.cov(x, θ, x_test)))
 
 plot(x, y, seriestype = :scatter, label = "y")
 plot!(x_test, mean_test, label = "mean")
