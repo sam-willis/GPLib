442e151e38d6726b95511eb0c929fea3e3a28ade
Sam <git@mail.samw.cc>
2022-03-18 00:22:40 +0000

    most of it working

--- GPR.jl
+++ GPR.jl
@@ -1,31 +1,64 @@
 using Flux
 using LinearAlgebra
 using Plots
+using TypedDelegation
+import Base
 
-#%% Data structures
-mutable struct GPRHyperParameters
-    σ:: Number
-    l:: Number
+abstract type Kernel end
+abstract type HyperParameters end
+abstract type GPModel end
+
+mutable struct HyperParameter{T <: Number} 
+    value:: T
+end
+Base.:+(x::HyperParameter, y::Number) = Base.:+(promote(x.value, y)...)
+Base.:+(x::Number, y::HyperParameter) = Base.:+(promote(x, y.value)...)
+Base.:-(x::HyperParameter, y::Number) = Base.:-(promote(x.value, y)...)
+Base.:-(x::Number, y::HyperParameter) = Base.:-(promote(x, y.value)...)
+Base.:*(x::HyperParameter, y::Number) = Base.:*(promote(x.value, y)...)
+Base.:*(x::Number, y::HyperParameter) = Base.:*(promote(x, y.value)...)
+Base.:/(x::HyperParameter, y::Number) = Base.:/(promote(x.value, y)...)
+Base.:/(x::Number, y::HyperParameter) = Base.:/(promote(x, y.value)...)
+Base.:^(x::HyperParameter, y::Number) = Base.:^(promote(x.value, y)...)
+Base.:^(x::Number, y::HyperParameter) = Base.:^(promote(x, y.value)...)
+Base.convert(::Type{T}, param:: HyperParameter{T}) where T = param.value
+Base.convert(::Type{HyperParameter}, value) = HyperParameter(value)
+
+#%% Data structuresQ
+struct RBFKernel <: Kernel
+    l:: HyperParameter
 end
 
-struct GPR
-    K:: Function
-    mean:: Function
-    cov:: Function
-    loglik:: Function
-    θ:: GPRHyperParameters
+(k:: RBFKernel)(x, y) = exp.(-abs.(x .- y').^2 ./ (2*k.l^2))
+
+struct GPR <: GPModel
+    kernel:: Kernel
+    σ:: HyperParameter
 end
 
 #%% Helper functions
-RBFKernel(x, y, θ) = exp.(-abs.(x .- y').^2 ./ (2*θ.l^2))
 
-function createGPR(K, θ)
-    # extend Kernel.θ with σ and make that GPR.θ
-    Kxx_noise(x, θ) = K(x, x, θ) + I*θ.σ^2
-    mean(x, y, θ, x_new) = K(x_new, x, θ) * inv(Kxx_noise(x, θ)) * y
-    cov(x, θ, x_new) = K(x_new, x_new, θ) - K(x_new, x, θ) * inv(Kxx_noise(x, θ)) * K(x_new, x, θ)'
-    loglik(x, y, θ) = -0.5*y'*inv(Kxx_noise(x, θ))*y - 0.5*log(det(Kxx_noise(x, θ))) - 0.5*length(x)*log(2*pi)
-    GPR(K, mean, cov, loglik, θ)
+function predict_mean(model:: GPR, x, y, x_new)
+    K = model.kernel
+    σ = model.σ
+    K(x_new, x) * inv(K(x, x) + I*σ^2) * y
+end
+
+function predict_cov(model:: GPR, x, x_new)
+    K = model.kernel
+    σ = model.σ
+    K(x_new, x_new) - K(x_new, x) * inv(K(x, x) + I*σ^2) * K(x_new, x)'
+end
+
+function predict_std(model:: GPModel, x, x_new)
+    sqrt.(diag(predict_cov(model, x, x_new)))
+end
+
+function loglik(model:: GPR, x, y)
+    K = model.kernel
+    σ = model.σ
+    n = length(x)
+    -0.5*y'*inv(K(x, x) + I*σ^2)*y - 0.5*log(det(K(x, x) + I*σ^2)) - 0.5*n*log(2*pi)
 end
 
 function grad_step!(g, θ, alpha)
@@ -38,9 +71,9 @@ function grad_step!(g, θ, alpha)
 end
 
 function optimise!(gpr:: GPR, x, y, n_steps=1000, alpha=0.001)
-    g(θ) = gradient(θ -> gpr.loglik(x, y, θ), θ)[1]
+    g(θ) = gradient(θ -> loglik(gpr, x, y), θ)
     for _=1:n_steps
-        grad_step!(g, gpr.θ, alpha)
+        grad_step!(g, gpr.σ, alpha)
     end
 end
 
@@ -49,18 +82,17 @@ x = collect(1:10);
 y = x/10 + 0.5 * rand(size(x, 1))
 x_test = collect(1:0.01:20)
 
-θ = GPRHyperParameters(0.1, 1) # initial parameters - we gotta start somewhere
-gpr = createGPR(RBFKernel, θ)
-println(θ)
+gpr = GPR(RBFKernel(1), 0.1)
 
 #%% basic optimisation
 
-optimise!(gpr, x, y)
-println(θ)
+println(gpr.σ)
+# optimise!(gpr, x, y)
+println(gpr.σ)
 
 #%% plot prediction vs training data
-mean_test = gpr.mean(x, y, θ, x_test)
-std_test = sqrt.(diag(gpr.cov(x, θ, x_test)))
+mean_test = predict_mean(gpr, x, y, x_test)
+std_test = predict_std(gpr, x, x_test)
 
 plot(x, y, seriestype = :scatter, label = "y")
 plot!(x_test, mean_test, label = "mean")
